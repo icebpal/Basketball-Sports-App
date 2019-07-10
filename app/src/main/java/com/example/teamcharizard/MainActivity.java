da199213f9a3d04afb6f55661edfd5a994468ae0
First <xingyub2@illinois.edu>
2019-07-10 11:28:13 -0500

    link coach button to coach login

--- app/src/main/java/com/example/teamcharizard/MainActivity.java
+++ app/src/main/java/com/example/teamcharizard/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.teamcharizard;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button coachButton;
+    private Button parentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coachButton = (Button) findViewById(R.id.coach_button);
+        parentButton = (Button) findViewById(R.id.parent_button);
+        coachButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CoachLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
