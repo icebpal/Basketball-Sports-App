f03a1cd4ece1558477c4c93635ba53f755b9bb7c
First <xingyub2@illinois.edu>
2019-07-14 15:27:36 -0500

    start new game activity

--- app/src/main/java/com/example/teamcharizard/StartNewGameActivity.java
+++ app/src/main/java/com/example/teamcharizard/StartNewGameActivity.java
@@ -1,4 +1,27 @@
 package com.example.teamcharizard;
 
-public class StartNewGameActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class StartNewGameActivity extends AppCompatActivity {
+    private Button add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_new_game);
+        add = (Button) findViewById(R.id.addGame);
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), ViewGamesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
