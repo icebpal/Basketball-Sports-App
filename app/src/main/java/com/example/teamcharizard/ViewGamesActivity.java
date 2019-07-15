3156cb95f22dd79ec795c30eb32b00e8828932cc
xingyub2 <xingyub2@illinois.edu>
2019-07-15 17:07:34 -0500

    Replace ViewGamesActivity.java

--- app/src/main/java/com/example/teamcharizard/ViewGamesActivity.java
+++ app/src/main/java/com/example/teamcharizard/ViewGamesActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ViewGamesActivity extends AppCompatActivity {
     private Button game1_button;
@@ -19,6 +20,12 @@ public class ViewGamesActivity extends AppCompatActivity {
         game1_button = (Button)findViewById(R.id.game1);
         team_button = (Button)findViewById(R.id.team_button);
         add_game = (Button)findViewById(R.id.add_new_game);
+        Intent i = getIntent();
+        String left = i.getStringExtra("LEFT");
+        String right = i.getStringExtra("RIGHT");
+        if (left!= null && right!= null) {
+            Toast.makeText(getApplicationContext(), left + right, Toast.LENGTH_SHORT).show();
+        }
         game1_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
