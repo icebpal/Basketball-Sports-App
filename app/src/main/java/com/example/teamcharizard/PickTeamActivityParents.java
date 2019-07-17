706fc85a1cded72b397102cafedadd8454b7d130
darcychen2 <mengtong.chen@yahoo.com>
2019-07-17 10:54:34 -0500

    fix strings file

---
+++ app/src/main/java/com/example/teamcharizard/PickTeamActivityParents.java
@@ -0,0 +1,14 @@
+package com.example.teamcharizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickTeamActivityParents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_team_parents);
+    }
+}
