eb5b6d808e9ee9c37b38b62772deba3df6e92068
darcychen2 <mengtong.chen@yahoo.com>
2019-07-18 17:12:41 -0500

    modified CoachLoginActivity

--- app/src/main/java/com/example/teamcharizard/CoachLoginActivity.java
+++ app/src/main/java/com/example/teamcharizard/CoachLoginActivity.java
@@ -1,48 +1,23 @@
 package com.example.teamcharizard;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.EditText;
+
 
 public class CoachLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String entered_username;
-        String entered_password
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach_login);
+
+
 
 
         EditText username_input = findViewById(R.id.username_entry);
         EditText password_input = findViewById(R.id.password_entry);
 
-        username_input.setOnEditorActionListener(new View.OnEditorActionListenener(){
-            @Override
-            public void onEditorAction(){
 
-
-
-            }
-        });
-
-
-<<<<<<< HEAD
-=======
-
-
-
-        password_input.setOnEditorActionListener(new View.OnEditorActionListenener(){
-            @Override
-            public void onEditorAction(){
-
-            }
-        });
->>>>>>> 088ef72522fe413de60ea337e1c4541e18da7f42
     }
-
-
-
 }
