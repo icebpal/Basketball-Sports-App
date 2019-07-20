0f02edecfe89647e4edeebfbcc085189157df969
darcychen2 <mengtong.chen@yahoo.com>
2019-07-20 11:59:20 -0500

    solved overlapped fragments problem by setting background color of each fragment xml

--- app/src/main/java/com/example/teamcharizard/ViewTeamsActivity.java
+++ app/src/main/java/com/example/teamcharizard/ViewTeamsActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 public class ViewTeamsActivity extends AppCompatActivity {
 
@@ -20,15 +21,29 @@ public class ViewTeamsActivity extends AppCompatActivity {
 
         Fragment fr;
 
+//        if(view == findViewById(R.id.team1_button)){
+//            fr = new ViewTeamsFragmentOne();
+//        }else{
+//            fr = new ViewTeamsFragmentTwo();
+//        }
         if(view == findViewById(R.id.team1_button)){
             fr = new ViewTeamsFragmentOne();
-        }else{
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.fragment, fr);
+            transaction.commit();
+        }
+        if(view == findViewById(R.id.team2_button)){
             fr = new ViewTeamsFragmentTwo();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.fragment, fr);
+            transaction.commit();
         }
 
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.fragment, fr);
-        transaction.commit();
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction transaction = manager.beginTransaction();
+//        transaction.replace(R.id.fragment, fr);
+//        transaction.commit();
     }
 }
