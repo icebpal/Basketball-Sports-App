e4cd55b9780ff0ae475f491a359f882fdfadf072
darcychen2 <mengtong.chen@yahoo.com>
2019-07-21 20:37:54 -0500

    prettify the layout

--- app/src/main/java/com/example/teamcharizard/ViewTeamsActivity.java
+++ app/src/main/java/com/example/teamcharizard/ViewTeamsActivity.java
@@ -7,40 +7,39 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 
 public class ViewTeamsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fragment fr;
-        fr = new ViewTeamsFragmentOne();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.team_stats_frag_container, fr);
-        transaction.commit();
+//        Fragment fr;
+//        fr = new ViewTeamsFragmentOne();
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction transaction = manager.beginTransaction();
+//        transaction.add(R.id.team_stats_frag_container, fr);
+//        transaction.commit();
         setContentView(R.layout.activity_view_teams);
     }
 
-    public void selectFrag(View view){
-
-        Fragment fr;
-
-        if(view == findViewById(R.id.team1_button)){
-            fr = new ViewTeamsFragmentOne();
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.team_stats_frag_container, fr);
-            transaction.commit();
-        }
-        if(view == findViewById(R.id.team2_button)){
-            fr = new ViewTeamsFragmentTwo();
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.team_stats_frag_container, fr);
-            transaction.commit();
-        }
-
-    }
+//    public void selectFrag(View view){
+//
+//        Fragment fr;
+//
+//        if(view == findViewById(R.id.first_quarter_button)){
+//            fr = new ViewTeamsFragmentOne();
+//            FragmentManager manager = getSupportFragmentManager();
+//            FragmentTransaction transaction = manager.beginTransaction();
+//            transaction.replace(R.id.team_stats_frag_container, fr);
+//            transaction.commit();
+//        }
+//        if(view == findViewById(R.id.second_quarter_button)){
+//            fr = new ViewTeamsFragmentTwo();
+//            FragmentManager manager = getSupportFragmentManager();
+//            FragmentTransaction transaction = manager.beginTransaction();
+//            transaction.replace(R.id.team_stats_frag_container, fr);
+//            transaction.commit();
+//        }
+//
+//    }
 }
